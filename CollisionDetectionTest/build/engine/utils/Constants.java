3ce177381fe5f69ccf44283d1db40fba4f28422a
JGlass <jacob.glass@ymail.com>
2016-04-25 09:30:05 +0100

    Removed ant building.

--- CollisionDetectionTest/build/engine/utils/Constants.java
+++
@@ -1,5 +0,0 @@
-package engine.utils;
-
-public class Constants {
-	public static final double restitution = 0.70;
-}
--- CollisionDetectionTest/build/engine/utils/Reference.java
+++
@@ -1,7 +0,0 @@
-package engine.utils;
-
-public class Reference {
-	public static final String NAME = "Gravitation";
-	public static final String VERSION = "2";
-	public static final String AUTHOR = "JacoJazz";
-}
