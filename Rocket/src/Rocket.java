8ed1926d576a699a5d89c1a0560b6500a7f9fb3f
JGlass <jacob.glass@ymail.com>
2016-05-12 11:48:20 +0100

    Rocket and added tank!

--- Rocket/src/Rocket.java
+++ Rocket/src/Rocket.java
@@ -1,37 +1,64 @@
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import math.geom2d.Point2D;
-import math.geom2d.line.LineArc2D;
+import math.geom2d.curve.GeneralPath2D;
 import math.geom2d.line.LineSegment2D;
 import math.geom2d.polygon.SimplePolygon2D;
+import math.geom2d.spline.CubicBezierCurve2D;
 
 public class Rocket {
-	Point2D center = new Point2D(0, 0);
 	SimplePolygon2D main = new SimplePolygon2D();
 	SimplePolygon2D engine = new SimplePolygon2D();
-	LineArc2D leftNose = new LineArc2D(new Point2D(100, 100), new Point2D(200, 200), 30, 30);
+	CubicBezierCurve2D noseLeft = new CubicBezierCurve2D(new Point2D(5, 50), new Point2D(5, 40), new Point2D(25, 0), new Point2D(35, 0));
+	CubicBezierCurve2D noseRight = new CubicBezierCurve2D(new Point2D(65, 50), new Point2D(65, 40), new Point2D(45, 0), new Point2D(35, 0));
+	ArrayList<LineSegment2D> lineArray = new ArrayList<LineSegment2D>();
+	SimplePolygon2D rcsLeft = new SimplePolygon2D();
+	SimplePolygon2D rcsRight = new SimplePolygon2D();
+	GeneralPath2D shape = new GeneralPath2D();
+	Point2D center;
 
 	public Rocket() {
-		main.addVertex(new Point2D(30, 0));
-		main.addVertex(new Point2D(0, 50));
-		main.addVertex(new Point2D(0, 300));
-		main.addVertex(new Point2D(60, 300));
-		main.addVertex(new Point2D(60, 50));
-		engine.addVertex(new Point2D(10, 300));
-		engine.addVertex(new Point2D(2, 310));
-		engine.addVertex(new Point2D(58, 310));
-		engine.addVertex(new Point2D(50, 300));
+		main.addVertex(new Point2D(5, 50));
+		main.addVertex(new Point2D(5, 300));
+		main.addVertex(new Point2D(65, 300));
+		main.addVertex(new Point2D(65, 50));
+
+		rcsLeft.addVertex(new Point2D(5, 170));
+		rcsLeft.addVertex(new Point2D(0, 172));
+		rcsLeft.addVertex(new Point2D(0, 178));
+		rcsLeft.addVertex(new Point2D(5, 180));
+
+		rcsRight.addVertex(new Point2D(65, 170));
+		rcsRight.addVertex(new Point2D(70, 172));
+		rcsRight.addVertex(new Point2D(70, 178));
+		rcsRight.addVertex(new Point2D(65, 180));
+
+		engine.addVertex(new Point2D(15, 300));
+		engine.addVertex(new Point2D(7, 310));
+		engine.addVertex(new Point2D(63, 310));
+		engine.addVertex(new Point2D(55, 300));
+
+		lineArray.add(new LineSegment2D(new Point2D(5, 70), new Point2D(65, 70)));
+		lineArray.add(new LineSegment2D(new Point2D(5, 80), new Point2D(65, 80)));
+		lineArray.add(new LineSegment2D(new Point2D(5, 100), new Point2D(65, 100)));
+		lineArray.add(new LineSegment2D(new Point2D(5, 200), new Point2D(65, 200)));
+
+		center = main.centroid();
 	}
 
 	public void paint(Graphics2D g2d) {
-		g2d.setColor(Color.WHITE);
-		main.fill(g2d);
 		g2d.setColor(Color.BLACK);
 		main.draw(g2d);
-		g2d.setColor(Color.GRAY);
 		engine.fill(g2d);
-		g2d.setColor(Color.BLACK);
-		leftNose.draw(g2d);
+		for (Iterator<LineSegment2D> lineIterator = lineArray.iterator(); lineIterator.hasNext();) {
+			lineIterator.next().draw(g2d);
+		}
+		noseLeft.draw(g2d);
+		noseRight.draw(g2d);
+		rcsLeft.draw(g2d);
+		rcsRight.draw(g2d);
 	}
 }
--- Tanks/src/Game.java
+++ Tanks/src/Game.java
@@ -4,21 +4,68 @@ import java.awt.Graphics2D;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import math.geom2d.Point2D;
+import math.geom2d.polygon.SimplePolygon2D;
+
 public class Game extends JPanel {
 	private static final long serialVersionUID = 1L;
 	static final int TARGET_FPS = 60;
-	static JFrame frame = new JFrame("Rocket");
+	static JFrame frame = new JFrame("Tanks");
 	static int frames = 0;
 	static GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 	static int width = gd.getDisplayMode().getWidth();
 	static int height = gd.getDisplayMode().getHeight();
-	Rocket rocket = new Rocket();
+	SimplePolygon2D terrain = new SimplePolygon2D();
 
 	Game() {
+		generateTerrain();
+
+		addKeyListener(new KeyListener() {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_G) {
+					generateTerrain();
+				}
+			}
+
+			public void keyReleased(KeyEvent e) {
+
+			}
+
+			public void keyTyped(KeyEvent e) {
+			}
+		});
+		setFocusable(true);
+	}
+
+	void generateTerrain() {
+		terrain.clearVertices();
+		Random r = new Random();
+		Point2D previousPoint = new Point2D(0, height / 2);
+		terrain.addVertex(previousPoint);
+		for (int terrainWidth = 100; terrainWidth < width; terrainWidth += 100) {
+			if (r.nextInt(2) == 0 && previousPoint.getY() < height - 200 && previousPoint.getY() > 200) {
+				terrain.addVertex(new Point2D(terrainWidth, previousPoint.getY() + r.nextInt(200)));
+			} else if (r.nextInt(2) == 1 && previousPoint.getY() < height - 200 && previousPoint.getY() > 200) {
+				terrain.addVertex(new Point2D(terrainWidth, previousPoint.getY() - r.nextInt(200)));
+			} else {
+				terrain.addVertex(new Point2D(terrainWidth, previousPoint.getY()));
+			}
+		}
+		terrain.addVertex(new Point2D(width, height / 2));
+		terrain.addVertex(new Point2D(width, height));
+		terrain.addVertex(new Point2D(0, height));
+	}
+
+	double randDouble(double rangeMin, double rangeMax) {
+		Random r = new Random();
+		return rangeMin + (rangeMax - rangeMin) * r.nextDouble();
 	}
 
 	void update() {
@@ -30,7 +77,8 @@ public class Game extends JPanel {
 		Graphics2D g2d = (Graphics2D) g;
 		applyQualityRenderingHints(g2d);
 
-		rocket.paint(g2d);
+		g2d.setColor(new Color(0x00cc00));
+		terrain.fill(g2d);
 	}
 
 	public static void applyQualityRenderingHints(Graphics2D g2d) {
@@ -49,7 +97,6 @@ public class Game extends JPanel {
 
 		frame.add(game);
 		frame.setSize(width, height);
-		frame.setBackground(Color.BLACK);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setUndecorated(true);
